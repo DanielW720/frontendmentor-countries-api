a8ecbce79dba6837ac79bc6db9a523c50a069afa
DW Desktop <westerlund.da@gmail.com>
2023-06-17 12:51:25 +0200

    Adding Radix to the project

---
+++ app/Popover.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import * as RadixPopover from "@radix-ui/react-popover";
+
+export const Popover = () => {
+  return (
+    <RadixPopover.Root>
+      <RadixPopover.Trigger>More info</RadixPopover.Trigger>
+      <RadixPopover.Portal>
+        <RadixPopover.Content>
+          Some more info…
+          <RadixPopover.Arrow />
+        </RadixPopover.Content>
+      </RadixPopover.Portal>
+    </RadixPopover.Root>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import { CountryCard } from "./CountryCard";
 import "material-icons/iconfont/material-icons.css";
 import { Searchbar } from "./Searchbar";
 import { Filter } from "./Filter";
+import { Popover } from "./Popover";
 
 export default function Home() {
   return (
     <main className="min-h-screen pt-8">
       <Searchbar />
       <Filter />
+
+      {/* Trying out radix */}
+      <Popover />
 
       {/* Content */}
       <article className="flex flex-col items-center justify-start">
