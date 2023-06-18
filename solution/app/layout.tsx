65e397d8f0434c7596a43e9844ad4d7386b26555
DW Desktop <westerlund.da@gmail.com>
2023-06-18 22:14:11 +0200

    Parent folder as new root folder in local git repo

---
+++ solution/app/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import "./globals.css";
+import { Nunito_Sans } from "next/font/google";
+import { useState } from "react";
+import "material-icons/iconfont/material-icons.css";
+import { Header } from "./Header";
+
+const nunitoSans = Nunito_Sans({
+  weight: ["300", "600", "800"],
+  subsets: ["cyrillic"],
+});
+
+export const metadata = {
+  title: "Frontend Mentor Countries API app",
+  description:
+    "Frontend Mentor challange app with with the Countries REST API.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <html lang="en" className={`${darkTheme && "dark"}`}>
+      <body
+        className={`${nunitoSans.className} bg-white dark:bg-veryDarkBlueBG text-veryDarkBlueText dark:text-white`}
+      >
+        <Header darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <main className="min-h-screen pt-8">{children}</main>
+      </body>
+    </html>
+  );
+}
