99d3fe59b676d2f7ca94cbe79be3b8c9ab5623d9
DW Desktop <westerlund.da@gmail.com>
2023-07-01 23:07:02 +0200

    Detail image object-cover, lack-of-data catching

--- solution/app/[country]/CountryFlag.tsx
+++ solution/app/[country]/CountryFlag.tsx
@@ -3,13 +3,13 @@ import { Country } from "../types/country";
 
 export const CountryFlag = ({ flags }: { flags: Country["flags"] }) => {
   return (
-    <div className="relative xs:w-[350px] xs:h-[240px] w-[250px] h-[140px] lg:w-[380px] lg:h-[340px]">
+    <div className="relative xs:w-[350px] xs:h-[240px] w-[250px] h-[140px] lg:w-[450px] lg:h-[320px]">
       <Image
         src={flags.png}
         alt={flags.alt}
         fill
         sizes="(max-width: 350px) 100vw"
-        className="shadow-md"
+        className="shadow-md object-cover"
         placeholder="blur"
         blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkf71zFQAFIwJfy/HRLAAAAABJRU5ErkJggg=="
       />
--- solution/app/[country]/Details.tsx
+++ solution/app/[country]/Details.tsx
@@ -3,48 +3,58 @@ import { CountryFlag } from "./CountryFlag";
 
 export const Details = ({ country }: { country: Country }) => {
   // Extract native names
-  const nativeNames = Object.keys(country.name.nativeName).map((key) => {
-    const nativeName = (country.name.nativeName[key] = {
-      official: country.name.nativeName[key].official,
-      common: country.name.nativeName[key].common,
+  let nativeNames;
+  if (country.name.nativeName)
+    nativeNames = Object.keys(country.name.nativeName).map((key) => {
+      const nativeName = (country.name.nativeName[key] = {
+        official: country.name.nativeName[key].official,
+        common: country.name.nativeName[key].common,
+      });
+      return <span>{nativeName.common}</span>;
     });
-    return <span>{nativeName.common}</span>;
-  });
 
   // Extract currencies
-  const currencies = Object.keys(country.currencies).map((key) => {
-    const currency = (country.currencies[key] = {
-      name: country.currencies[key].name,
-      symbol: country.currencies[key].symbol,
+  let currencies;
+  if (country.currencies)
+    currencies = Object.keys(country.currencies).map((key) => {
+      const currency = (country.currencies[key] = {
+        name: country.currencies[key].name,
+        symbol: country.currencies[key].symbol,
+      });
+      return <span>{currency.name}</span>;
     });
-    return <span>{currency.name}</span>;
-  });
 
   // Extract languages
-  const languages = Object.keys(country.languages).map((key) => (
-    <span>{country.languages[key]}</span>
-  ));
+  let languages;
+  if (country.languages)
+    languages = Object.keys(country.languages).map((key) => (
+      <span>{country.languages[key]}</span>
+    ));
 
   // Extract bordering countries ()
-  const bordersFullnames = country.bordersFullnames?.map((borderCountry) => (
-    <div
-      key={borderCountry}
-      className="py-1 px-4 shadow-lgSymmetric font-semibold w-28 flex justify-center items-center text-sm rounded-sm dark:bg-darkBlue"
-    >
-      {borderCountry}
-    </div>
-  ));
+  let bordersFullnames;
+  if (country.bordersFullnames)
+    bordersFullnames = country.bordersFullnames?.map((borderCountry) => (
+      <div
+        key={borderCountry}
+        className="py-1 px-4 shadow-lgSymmetric font-semibold w-28 flex justify-center items-center text-sm rounded-sm dark:bg-darkBlue"
+      >
+        {borderCountry}
+      </div>
+    ));
 
   return (
     <article className="grid grid-cols-1 lg:grid-cols-oneThirdTwoThirds gap-10 justify-items-start mt-16 w-full lg:gap-0">
       <CountryFlag flags={country.flags} />
       <div className="grid grid-cols-1 lg:grid-cols-2 w-full">
-        <h2 className="text-3xl font-bold lg:col-start-1 lg:col-end-3 lg:mt-4">
+        <h2 className="text-3xl font-bold lg:col-start-1 lg:col-end-3 lg:mt-2">
           {country.name.common}
         </h2>
         <ul className="my-6">
           <li className="my-2">
-            <p className="font-semibold">Native Name: {nativeNames[0]}</p>
+            <p className="font-semibold">
+              Native Name: {nativeNames ? nativeNames[0] : "None"}
+            </p>
           </li>
           <li className="my-2">
             <p className="font-semibold">Population: {country.population}</p>
@@ -69,10 +79,14 @@ export const Details = ({ country }: { country: Country }) => {
             </p>
           </li>
           <li className="my-2">
-            <p className="font-semibold">Currencies: {currencies}</p>
+            <p className="font-semibold">
+              Currencies: {currencies ? currencies : "None"}
+            </p>
           </li>
           <li className="my-2">
-            <p className="font-semibold">Languages: {languages}</p>
+            <p className="font-semibold">
+              Languages: {languages ? languages : "None"}
+            </p>
           </li>
         </ul>
         <div className="flex flex-col lg:flex-row lg:items-center justify-center lg:justify-start mt-6 lg:mt-0 lg:col-start-1 lg:col-end-3">
