ddc309931e25585b7a9e6cd21e2265f1e096e431
DW Desktop <westerlund.da@gmail.com>
2023-07-08 13:43:26 +0200

    Search-as-you-type feature

--- solution/app/Searchbar.tsx
+++ solution/app/Searchbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Region } from "./CountryList";
 
@@ -25,13 +25,26 @@ export const Searchbar = ({
     watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) =>
-    filterCountries({ query: new RegExp(data.query, "i") });
+  const onSubmit: SubmitHandler<Inputs> = (data) => filterAndSearch(data.query);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const query = watch("query");
+      filterAndSearch(query);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [watch("query")]);
+
+  function filterAndSearch(query: string) {
+    filterCountries({ query: new RegExp(query, "i") });
+  }
 
   return (
     <div className="w-full max-w-[26rem]">
       <form
         onSubmit={handleSubmit(onSubmit)}
+        autoComplete="off"
         className="shadow-mdSymmetric h-16 flex flex-row items-center justify-start dark:bg-darkBlue rounded-md"
       >
         <button type="submit">
@@ -42,7 +55,7 @@ export const Searchbar = ({
           type="text"
           {...register("query")}
           placeholder="Search for a country..."
-          className="bg-inherit outline-none overflow-hidden w-full text-blue-600 dark:text-blue-300 font-semibold dark:caret-white caret-black"
+          className="bg-inherit outline-none overflow-hidden w-full tracking-wide text-blue-600 dark:text-blue-300 font-semibold dark:caret-white caret-black appearance-none"
         />
         <input type="submit" className="hidden" />
       </form>
