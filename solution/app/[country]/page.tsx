1e2e3ed90621f22a127ea70279317a57b8da4dd1
DW Desktop <westerlund.da@gmail.com>
2023-07-12 21:55:26 +0200

    Ditching /api because this is next.js 13 and my app do not need new API endpoints

--- solution/app/[country]/page.tsx
+++ solution/app/[country]/page.tsx
@@ -1,14 +1,8 @@
 import "material-icons/iconfont/material-icons.css";
 import { Backbutton } from "./Backbutton";
 import { Details } from "./Details";
+import { NextResponse } from "next/server";
 import { Country } from "../types/country";
-
-async function fetchCountry(country: string): Promise<Country> {
-  const res = await fetch(`http://localhost:3000/api/countries/${country}`, {
-    cache: "no-store",
-  });
-  return res.json();
-}
 
 export default async function page({
   params,
@@ -25,3 +19,57 @@ export default async function page({
     </div>
   );
 }
+
+/**
+ * Fetch a country.
+ * @param request NextRequest
+ * @returns Promise<NextResponse<Country>>
+ */
+export async function fetchCountry(country: string): Promise<Country> {
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Couldn't fetch country");
+
+  const countryJson: Country[] = await res.json();
+
+  const borderCodes = countryJson[0].borders;
+  if (borderCodes) {
+    const bordersFullnames = await fetchBorderCountryNames(borderCodes);
+    countryJson[0].bordersFullnames = await bordersFullnames.json();
+  }
+
+  // return countryJson[0];
+  return countryJson[0];
+}
+
+/**
+ * Fetch the full names of the countries.
+ * @param borderCodes The list of border codes.
+ * @returns Promise<NextResponse<string[]>>
+ */
+async function fetchBorderCountryNames(
+  borderCodes: string[]
+): Promise<NextResponse<string[]>> {
+  try {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/alpha?fields=name&codes=${borderCodes}`
+    );
+    const borderCountries: BorderCountries[] = await res.json();
+    const borderNames: Country["borders"] = [];
+    borderCountries.forEach((borderCountry) =>
+      borderNames.push(borderCountry.name.common)
+    );
+    return NextResponse.json(borderNames);
+  } catch (e) {
+    throw new Error("Couldn't fetch border countries");
+  }
+}
+
+type BorderCountries = {
+  name: {
+    common: "Norway";
+    official: "Kingdom of Norway";
+  };
+};
--- solution/app/page.tsx
+++ solution/app/page.tsx
@@ -8,16 +8,78 @@ export const metadata = {
     "Frontend Mentor challange app with with the Countries REST API.",
 };
 
-async function fetchCountries() {
-  const data = await fetch("http://localhost:3000/api/countries");
-  return data.json();
-}
+const DATA_SOURCE_URL = "https://restcountries.com/v3.1/all";
 
 export default async function page() {
   const data: Country[] = await fetchCountries();
-
-  // const countries = data.slice(0, 9);
   const countries = data;
 
   return <CountryList countryList={countries} />;
+}
+
+/**
+ * Fetch all countries.
+ * @returns Promise<NextResponse<any>>
+ */
+export async function fetchCountries() {
+  const res = await fetch(DATA_SOURCE_URL);
+
+  if (!res.ok) throw new Error("Couldn't fetch countries");
+
+  const resJson: Country[] = await res.json();
+
+  const countries: Country[] = [];
+  resJson.forEach((country) => {
+    // Iterating over dynamic keys for currencies
+    const currencies = {} as Country["currencies"];
+    if (country.currencies) {
+      Object.keys(country.currencies).forEach((key) => {
+        currencies[key] = {
+          name: country.currencies[key].name,
+          symbol: country.currencies[key].symbol,
+        };
+      });
+    }
+
+    // Iterating over dynamic keys for nativeNames
+    const nativeName = {} as Country["name"]["nativeName"];
+    if (country.name.nativeName) {
+      Object.keys(country.name.nativeName).forEach((key) => {
+        nativeName[key] = {
+          official: country.name.nativeName[key].official,
+          common: country.name.nativeName[key].common,
+        };
+      });
+    }
+
+    // Iterating over dynamic keys for languages
+    const languages = {} as Country["languages"];
+    if (country.languages) {
+      Object.keys(country.languages).forEach((key) => {
+        languages[key] = country.languages[key];
+      });
+    }
+
+    countries.push({
+      flags: {
+        png: country.flags.png,
+        alt: country.flags.alt,
+      },
+      population: country.population,
+      region: country.region,
+      subregion: country.subregion,
+      capital: country.capital,
+      tld: country.tld,
+      currencies,
+      languages,
+      borders: country.borders,
+      name: {
+        common: country.name.common,
+        official: country.name.official,
+        nativeName,
+      },
+    });
+  });
+
+  return countries;
 }
